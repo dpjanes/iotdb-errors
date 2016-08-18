d4419f2914039b211b18aed15fba294123941899
David Janes <davidjanes@davidjanes.com>
2016-08-18 12:34:45 -0400

    100% test coverage

--- test/test_errors.js
+++ test/test_errors.js
@@ -22,140 +22,289 @@
 
 "use strict";
 
-const errors = require('../errors');
 const assert = require('assert');
+const iotdb = require('iotdb');
+const _ = iotdb._;
+
+const errors = require('../errors');
 
 const _my_message = "my message";
 
 describe('test_errors', function() {
-    it('NotFound', function() {
-        const error_class = errors.NotFound;
-        const error = new error_class(_my_message);
+    describe('own message', function() {
+        it('NotFound', function() {
+            const error_class = errors.NotFound;
+            const error = new error_class(_my_message);
 
-        assert.strictEqual(error.message, _my_message);
-        assert.ok(error instanceof error_class);
-        assert.ok(error instanceof Error);
-        assert.strictEqual(error.code, 404);
+            assert.strictEqual(error.message, _my_message);
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 404);
+        });
+
+
+        it('NotAuthorized', function() {
+            const error_class = errors.NotAuthorized;
+            const error = new error_class(_my_message);
+
+            assert.strictEqual(error.message, _my_message);
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 401);
+        });
+
+
+        it('Timestamp', function() {
+            const error_class = errors.Timestamp;
+            const error = new error_class(_my_message);
+
+            assert.strictEqual(error.message, _my_message);
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 409);
+        });
+
+
+        it('NotAppropriate', function() {
+            const error_class = errors.NotAppropriate;
+            const error = new error_class(_my_message);
+
+            assert.strictEqual(error.message, _my_message);
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 403);
+        });
+
+
+        it('Invalid', function() {
+            const error_class = errors.Invalid;
+            const error = new error_class(_my_message);
+
+            assert.strictEqual(error.message, _my_message);
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 403);
+        });
+
+
+        it('MethodNotAllowed', function() {
+            const error_class = errors.MethodNotAllowed;
+            const error = new error_class(_my_message);
+
+            assert.strictEqual(error.message, _my_message);
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 405);
+        });
+
+
+        it('ServiceNotAvailable', function() {
+            const error_class = errors.ServiceNotAvailable;
+            const error = new error_class(_my_message);
+
+            assert.strictEqual(error.message, _my_message);
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 503);
+        });
+
+
+        it('NotImplemented', function() {
+            const error_class = errors.NotImplemented;
+            const error = new error_class(_my_message);
+
+            assert.strictEqual(error.message, _my_message);
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 501);
+        });
+
+
+        it('ShouldBeImplementedInSubclass', function() {
+            const error_class = errors.ShouldBeImplementedInSubclass;
+            const error = new error_class(_my_message);
+
+            assert.strictEqual(error.message, _my_message);
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 501);
+        });
+
+
+        it('NeverImplemented', function() {
+            const error_class = errors.NeverImplemented;
+            const error = new error_class(_my_message);
+
+            assert.strictEqual(error.message, _my_message);
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 501);
+        });
+
+
+        it('SetupRequired', function() {
+            const error_class = errors.SetupRequired;
+            const error = new error_class(_my_message);
+
+            assert.strictEqual(error.message, _my_message);
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 500);
+        });
+
+
+        it('Internal', function() {
+            const error_class = errors.Internal;
+            const error = new error_class(_my_message);
+
+            assert.strictEqual(error.message, _my_message);
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 500);
+        });
     });
+    describe('native message', function() {
+        it('NotFound', function() {
+            const error_class = errors.NotFound;
+            const error = new error_class();
+
+            assert.ok(error.message !== _my_message);
+            assert.ok(_.is.String(error.message));
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 404);
+        });
 
 
-    it('NotAuthorized', function() {
-        const error_class = errors.NotAuthorized;
-        const error = new error_class(_my_message);
+        it('NotAuthorized', function() {
+            const error_class = errors.NotAuthorized;
+            const error = new error_class();
 
-        assert.strictEqual(error.message, _my_message);
-        assert.ok(error instanceof error_class);
-        assert.ok(error instanceof Error);
-        assert.strictEqual(error.code, 401);
-    });
-
-
-    it('Timestamp', function() {
-        const error_class = errors.Timestamp;
-        const error = new error_class(_my_message);
-
-        assert.strictEqual(error.message, _my_message);
-        assert.ok(error instanceof error_class);
-        assert.ok(error instanceof Error);
-        assert.strictEqual(error.code, 409);
-    });
+            assert.ok(error.message !== _my_message);
+            assert.ok(_.is.String(error.message));
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 401);
+        });
 
 
-    it('NotAppropriate', function() {
-        const error_class = errors.NotAppropriate;
-        const error = new error_class(_my_message);
+        it('Timestamp', function() {
+            const error_class = errors.Timestamp;
+            const error = new error_class();
 
-        assert.strictEqual(error.message, _my_message);
-        assert.ok(error instanceof error_class);
-        assert.ok(error instanceof Error);
-        assert.strictEqual(error.code, 403);
-    });
-
-
-    it('Invalid', function() {
-        const error_class = errors.Invalid;
-        const error = new error_class(_my_message);
-
-        assert.strictEqual(error.message, _my_message);
-        assert.ok(error instanceof error_class);
-        assert.ok(error instanceof Error);
-        assert.strictEqual(error.code, 403);
-    });
+            assert.ok(error.message !== _my_message);
+            assert.ok(_.is.String(error.message));
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 409);
+        });
 
 
-    it('MethodNotAllowed', function() {
-        const error_class = errors.MethodNotAllowed;
-        const error = new error_class(_my_message);
+        it('NotAppropriate', function() {
+            const error_class = errors.NotAppropriate;
+            const error = new error_class();
 
-        assert.strictEqual(error.message, _my_message);
-        assert.ok(error instanceof error_class);
-        assert.ok(error instanceof Error);
-        assert.strictEqual(error.code, 405);
-    });
-
-
-    it('ServiceNotAvailable', function() {
-        const error_class = errors.ServiceNotAvailable;
-        const error = new error_class(_my_message);
-
-        assert.strictEqual(error.message, _my_message);
-        assert.ok(error instanceof error_class);
-        assert.ok(error instanceof Error);
-        assert.strictEqual(error.code, 503);
-    });
+            assert.ok(error.message !== _my_message);
+            assert.ok(_.is.String(error.message));
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 403);
+        });
 
 
-    it('NotImplemented', function() {
-        const error_class = errors.NotImplemented;
-        const error = new error_class(_my_message);
+        it('Invalid', function() {
+            const error_class = errors.Invalid;
+            const error = new error_class();
 
-        assert.strictEqual(error.message, _my_message);
-        assert.ok(error instanceof error_class);
-        assert.ok(error instanceof Error);
-        assert.strictEqual(error.code, 501);
-    });
-
-
-    it('ShouldBeImplementedInSubclass', function() {
-        const error_class = errors.ShouldBeImplementedInSubclass;
-        const error = new error_class(_my_message);
-
-        assert.strictEqual(error.message, _my_message);
-        assert.ok(error instanceof error_class);
-        assert.ok(error instanceof Error);
-        assert.strictEqual(error.code, 501);
-    });
+            assert.ok(error.message !== _my_message);
+            assert.ok(_.is.String(error.message));
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 403);
+        });
 
 
-    it('NeverImplemented', function() {
-        const error_class = errors.NeverImplemented;
-        const error = new error_class(_my_message);
+        it('MethodNotAllowed', function() {
+            const error_class = errors.MethodNotAllowed;
+            const error = new error_class();
 
-        assert.strictEqual(error.message, _my_message);
-        assert.ok(error instanceof error_class);
-        assert.ok(error instanceof Error);
-        assert.strictEqual(error.code, 501);
-    });
-
-
-    it('SetupRequired', function() {
-        const error_class = errors.SetupRequired;
-        const error = new error_class(_my_message);
-
-        assert.strictEqual(error.message, _my_message);
-        assert.ok(error instanceof error_class);
-        assert.ok(error instanceof Error);
-        assert.strictEqual(error.code, 500);
-    });
+            assert.ok(error.message !== _my_message);
+            assert.ok(_.is.String(error.message));
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 405);
+        });
 
 
-    it('Internal', function() {
-        const error_class = errors.Internal;
-        const error = new error_class(_my_message);
+        it('ServiceNotAvailable', function() {
+            const error_class = errors.ServiceNotAvailable;
+            const error = new error_class();
 
-        assert.strictEqual(error.message, _my_message);
-        assert.ok(error instanceof error_class);
-        assert.ok(error instanceof Error);
-        assert.strictEqual(error.code, 500);
+            assert.ok(error.message !== _my_message);
+            assert.ok(_.is.String(error.message));
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 503);
+        });
+
+
+        it('NotImplemented', function() {
+            const error_class = errors.NotImplemented;
+            const error = new error_class();
+
+            assert.ok(error.message !== _my_message);
+            assert.ok(_.is.String(error.message));
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 501);
+        });
+
+
+        it('ShouldBeImplementedInSubclass', function() {
+            const error_class = errors.ShouldBeImplementedInSubclass;
+            const error = new error_class();
+
+            assert.ok(error.message !== _my_message);
+            assert.ok(_.is.String(error.message));
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 501);
+        });
+
+
+        it('NeverImplemented', function() {
+            const error_class = errors.NeverImplemented;
+            const error = new error_class();
+
+            assert.ok(error.message !== _my_message);
+            assert.ok(_.is.String(error.message));
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 501);
+        });
+
+
+        it('SetupRequired', function() {
+            const error_class = errors.SetupRequired;
+            const error = new error_class();
+
+            assert.ok(error.message !== _my_message);
+            assert.ok(_.is.String(error.message));
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 500);
+        });
+
+
+        it('Internal', function() {
+            const error_class = errors.Internal;
+            const error = new error_class();
+
+            assert.ok(error.message !== _my_message);
+            assert.ok(_.is.String(error.message));
+            assert.ok(error instanceof error_class);
+            assert.ok(error instanceof Error);
+            assert.strictEqual(error.code, 500);
+        });
     });
 });
